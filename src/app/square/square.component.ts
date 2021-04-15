65dda6850c57680c7ea0e02bab946f1132e3de53
Shivrajsinh Rana <shivrajrana@Shivrajsinhs-MacBook-Air.local>
2021-04-15 19:29:11 +0530

    Nebular added

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-square',
@@ -7,7 +7,8 @@ import { Component} from '@angular/core';
 })
 export class SquareComponent {
 
+    
 
-  }
+}
 
 
