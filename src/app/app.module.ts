001c08e8b8f0856b794a7fbd8480020e4c60d73d
Shivrajsinh Rana <shivrajrana@Shivrajsinhs-MacBook-Air.local>
2021-04-15 20:21:48 +0530

    Demo Working PWA V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 //import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,12 @@ import { environment } from '../environments/environment';
     NbLayoutModule,
     NbEvaIconsModule,
     NbButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     //ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment. production})
   ],
   providers: [],
